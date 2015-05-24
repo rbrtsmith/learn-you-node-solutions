ff6dd01db4cc7b9d26a6b04741d43af600f34648
Robert Smith <rbrtsmith84@googlemail.com>
2015-05-24 14:06:29 +0200

    Update app.js

--- tut04/app.js
+++ tut04/app.js
@@ -1,5 +1,9 @@
 var fs = require('fs');
-var filename = process.argv[2];
-fs.readFile(filename, function(err, data) {
-    console.log(data.toString().split('\n').length-1);
+var file = process.argv[2];
+fs.readFile(file, function(err, data) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data.toString().split('\n').length-1); 
+    }
 });
