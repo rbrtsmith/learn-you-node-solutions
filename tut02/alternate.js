f07e5001801b61a5ad8a983da4f3f52e4a0b8816
Robert Smith <rbrtsmith84@googlemail.com>
2015-05-24 13:49:20 +0200

    Update alternate.js

--- tut02/alternate.js
+++ tut02/alternate.js
@@ -2,3 +2,12 @@ var args = process.argv.slice(2);
 console.log(args.reduce(function(prevVal, currVal) {
     return +prevVal + +currVal;
 }));
+
+/*
+#1: Remove first two elements of array returned from process.argv and store it in args variable
+#2: console.log returned value from the reduce function that is called on the args array.
+-> Reduce function when called on an array reduces it to a single value based on the callback function that passes over
+  each element.  Here we use it to get the total value of the stored numbers
+#3 the + before the value coerces the string into a number.  shorthand for: parseInt(num, 10);
+
+*/
